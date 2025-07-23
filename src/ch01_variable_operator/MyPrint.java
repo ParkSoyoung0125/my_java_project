50a6fec9991acfda4d425ddaea923528ac2ecb36
ParkSoyoung <hellene125@gmail.com>
2025-07-23 12:06:36 +0900

    First commit

--- src/ch01_variable_operator/MyPrint.java
+++ src/ch01_variable_operator/MyPrint.java
@@ -1,4 +1,34 @@
 package ch01_variable_operator;
-
+/*
+    작성자 : 홍길동
+    작성일 : 25년 7월 23일
+    목적 : 처음 해보는 자바프로그래밍
+*/
 public class MyPrint {
+    public static void main(String[] args) {
+        System.out.println("홍");
+        System.out.println("길");
+        System.out.println("동");
+
+        System.out.println();   // 줄바꿈
+
+        System.out.print("대한 민국"); // 줄바꿈 없음
+        System.out.println(" 화이팅");// 줄바꿈 있음
+
+        System.out.println();
+
+        System.out.println("\'예체능\' 프로그램 메인 MC \"강호동\"");
+
+        System.out.println();
+
+        System.out.println("이름\t\t국어\t영어\t수학");
+        System.out.println("김철수\t50\t60\t70");
+        System.out.println("김영희\t30\t40\t50");
+
+        // 한줄짜리 주석(comment)
+
+        /*여러줄 쓸수 있는 주석
+        줄바꿈해도 가능*/
+
+    }
 }
